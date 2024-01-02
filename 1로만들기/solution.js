4a663515df56d86124a75d6f5fc52fe1d6bb3217
yh.cho <youngho7482@gmail.com>
2024-01-02 18:23:07 +0900

    20240102_1이되는수_수정2

--- 1로만들기/solution.js
+++ 1로만들기/solution.js
@@ -1,19 +1,19 @@
 function solution(num_list) {
-    let cnt = 0 // num_list의 원소들이 1이될때까지 카운트할 변수
+    let cnt = 0; // num_list의 원소들이 1이될때까지 카운트할 변수
     for (let i = 0; i < num_list.length; i++){ // num_list 배열을 반복하고
         while(true) {
             if (num_list[i] === 1){ // number가 1이 되면 반복종료
-                break
+                break;
             }
             if (num_list[i] % 2 === 0){ // 짝수일 때
-                num_list[i] = num_list[i] / 2
+                num_list[i] = num_list[i] / 2;
                 cnt += 1 // 카운트 1
             }else{ // 홀수일 때
-                num_list[i] = (num_list[i] - 1) / 2
-                cnt += 1 // 카운트 1
+                num_list[i] = (num_list[i] - 1) / 2;
+                cnt += 1; // 카운트 1
             }
         }
     }
-    return cnt
+    return cnt;
 }
 console.log(solution([12,4,15,1,14]));
