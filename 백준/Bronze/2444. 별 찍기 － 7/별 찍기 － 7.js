20fa5c9e9161920a25878df4bd23d2ea0ff393ba
yh.cho <91147281+ohofront@users.noreply.github.com>
2025-02-22 08:28:11 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 104 ms, Memory: 9668 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.js
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.js
@@ -1,17 +1,17 @@
-/* 1. N을 fs module을 통해 입력 받고 Number type으로 변환 */
+/* 1. fs 모듈을 사용하여 입력 값을 읽고 숫자로 변환 */
 const fs = require('fs');
 const N = Number(fs.readFileSync('/dev/stdin'));
 
-/* 2. 1부터 N-1까지 공백과 별 찍기 */
-for(let i=1; i < N; i++){
-    let blank = ' '.repeat( (N-i) );
-    let stars = '*'.repeat( i+(i-1) );
-    console.log( blank + stars );
+/* 2. 위쪽 삼각형 (1부터 N-1까지) */
+for (let i = 1; i < N; i++) {
+    let blank = ' '.repeat(N - i);  // 왼쪽 공백 (N-i 개)
+    let stars = '*'.repeat(i + (i - 1));  // 별 개수 (2*i - 1 개)
+    console.log(blank + stars);  // 공백 + 별 출력
 }
 
-/* 3. N부터 1까지 공백과 별 찍기 */
-for(let j=N; j > 0; j--) {
-    let blank = ' '.repeat( (N-j) );
-    let stars = '*'.repeat( j+(j-1) );
-    console.log( blank + stars );
+/* 3. 아래쪽 삼각형 (N부터 1까지) */
+for (let j = N; j > 0; j--) {
+    let blank = ' '.repeat(N - j);  // 왼쪽 공백 (N-j 개)
+    let stars = '*'.repeat(j + (j - 1));  // 별 개수 (2*j - 1 개)
+    console.log(blank + stars);  // 공백 + 별 출력
 }
